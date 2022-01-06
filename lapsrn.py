f2af4e06f367e068ebe0e9fe4f5b1c4e43ecb9f6
Rick <rsulisthio@gmail.com>
2022-01-06 16:05:58 +0700

    change relu to LeakyReLU

--- lapsrn.py
+++ lapsrn.py
@@ -1,56 +1,57 @@
 import jittor as jt
+from jittor import nn
 
 class _Conv_Block(jt.Module):
     def __init__(self):
         super().__init__()
         
-        self.cov_block = jt.jt.jt.nn.Sequential(
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
-            jt.nn.ConvTranspose(in_channels=64, out_channels=64, kernel_size=4, stride=2, padding=1, bias=False),
-            jt.nn.relu(0.2, inplace=True),
+        self.cov_block = jt.jt.nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.ConvTranspose(in_channels=64, out_channels=64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
         )
         
     def forward(self, x):
         out = self.cov_block(x)
         return out
 
-class Net(jt.nn.Module):
+class Net(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.conv_input = jt.nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.relu = jt.nn.relu(0.2, inplace=True)
-        self.convt_I1 = jt.nn.ConvTranspose(1, 1, kernel_size=4, stride=2, padding=1, bias=False)
-        self.convt_R1 = jt.nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv_input = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu = nn.relu(0.2, inplace=True)
+        self.convt_I1 = nn.ConvTranspose(1, 1, kernel_size=4, stride=2, padding=1, bias=False)
+        self.convt_R1 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1, bias=False)
         self.convt_F1 = self.make_layer(_Conv_Block)
         
-        self.convt_I2 = jt.nn.ConvTranspose(1, 1, kernel_size=4, stride=2, padding=1, bias=False)
-        self.convt_R2 = jt.nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.convt_I2 = nn.ConvTranspose(1, 1, kernel_size=4, stride=2, padding=1, bias=False)
+        self.convt_R2 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1, bias=False)
         self.convt_F2 = self.make_layer(_Conv_Block)
         
     def make_layer(self, block):
         layers = []
         layers.append(block())
-        return jt.nn.Sequential(*layers)
+        return nn.Sequential(*layers)
     
     def forward(self, x):    
         out = self.relu(self.conv_input(x))
