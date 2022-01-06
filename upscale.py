23afbf2c44452d9a8d08092c89f131bd819be552
Rick <rsulisthio@gmail.com>
2022-01-06 16:30:56 +0700

    change pil to opencv

--- upscale.py
+++ upscale.py
@@ -3,17 +3,17 @@ from PIL import Image
 import jittor as jt
 import jittor.transform as transform
 import numpy as np
+import cv2
 
 def upscale(name, factor):
-	img = Image.open(str(name))
-	img = img.convert('L')				# transform to grayscale
+	img = cv2.imread(".'/Bromo_2.jpg", cv2.IMREAD_GRAYSCALE)
 
 	# load pretrained model
 	cnn = Net()
 	cnn.load("./model_best.pkl")
 
 	# run lapSRN
-	img_arr = np.asarray(img).astype("float")
+	img_arr = img.astype(float)
 	img_arr = img_arr / 255.0
 	input_var = jt.Var(img_arr).float().view(1, -1, img_arr.shape[0], img_arr.shape[1])
 	HR_2x, HR_4x = cnn(input_var)
