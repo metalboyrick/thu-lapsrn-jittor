262e2938e7002c7be6ed5794993fd716514dd5c5
Rick <rsulisthio@gmail.com>
2022-01-06 16:39:06 +0700

    no message

--- upscale.py
+++ upscale.py
@@ -21,7 +21,7 @@ def upscale(name, factor):
 	hi_res = jt.squeeze(HR_4x, 0)
 	hires_img = hi_res
 
-	cv2.imshow("test", hires_img)
+	cv2_imshow("test", hires_img)
 	# return the image
 
 def main():
